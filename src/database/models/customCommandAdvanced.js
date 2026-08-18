80f3aa64a2dd22b9bf2aac734ee7c36c548d54d1
R3nYamashiro <eoeviege@gmail.com>
2026-08-18 15:04:30 +0800

    Create customCommandAdvanced.js

---
+++ src/database/models/customCommandAdvanced.js
@@ -0,0 +1,9 @@
+
+const Schema = new mongoose.Schema({
+    Guild: String,
+    Name: String,
+    Responce: String,
+    Action: { type: String, default: "Normal" },
+});
+
+module.exports = mongoose.model("customCommandsAdvanced", Schema);
